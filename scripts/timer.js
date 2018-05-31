b38fbf8fb8a61c92057e438fde2d56773714057d
Curtis Robinson <38477322+curt60@users.noreply.github.com>
2018-05-30 17:12:14 -0700

    timer and misc updates

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,12 +1,33 @@
 //test time alert functionality
 
 var expWindow = function() {
-    var expWindow = window.open("", "", "width=200, height=100");   // Opens a new window
-    expWindow.document.write("<p>Time for a break!</p>");         // Some text in the new window
-    expWindow.focus();  
+    //var expWindow = window.open("timer-pop.html", "_blank", "width=400, height=200, menubar=no, resizeable=no, scrollbar=no, status=no, title=no, top=200px, left=400px");   // Opens a new window
+    //expWindow.focus();
+    
+    var notificationOptions = {
+        body: "Take a break!",
+        icon: "./favicon.png",
+        badge: "./favicon.png",
+        image: "./images/favicon_128x128.png",
+        requireInteraction: true
+    }
+    var expNotification = new Notification("Text Utilities", notificationOptions);  
+    
+    expNotification.onclick = function(event){
+        event.preventDefault();
+        this.close();
+
+    }
+
+
 }
 
 var startTimer = function(mins) {
+    if (mins == 30) document.getElementById("countdown").innerHTML = "0 hrs 29 mins";
+    else if (mins == 60) document.getElementById("countdown").innerHTML = "0 hrs 59 mins";
+    else if (mins == 120) document.getElementById("countdown").innerHTML = "1 hrs 59 mins";
+    else document.getElementById("countdown").innerHTML = "Timer Started";
+    
     var expTime = new Date().getTime() + (mins * 60 * 1000);
     
     var breakTimer = setInterval(function() {
